29f0547fceefd82b07778c3b77c2772518e75113
Rg <remi.grincourt@ens-lyon.fr>
2025-03-17 11:02:10 +0100

    Changed the find bonds function to only do singular bonds. Still not working really well, will look into further in the future

--- rgmol/general_function.py
+++ rgmol/general_function.py
@@ -46,7 +46,7 @@ dict_electronegativity = {
 }
 
 
-def find_atom_near(molecule,pos,factor_threshold=1.2):
+def find_atom_near(molecule,pos,factor_threshold=1.5):
     """
     function that returns the atoms that are near the one of interest
     """
@@ -97,84 +97,95 @@ def is_phenyl(mol,atoms_near):
 
 
 
-def find_bonds(mol):
+def find_bonds(mol,mode=0):
     """
     Function that tries to find the bonds of a molecule
     This script is still in WIP
     """
-
     bonds = []
-    number_bonds = [10 for k in range(len(mol.atoms))]
-    list_atom_near = []
-    list_is_phenyl = []
 
-    #Find number of bonds for each atom
-    for atom_x in range(len(mol.atoms)):
-        if mol.atoms[atom_x].name in dict_number_bonds:
-            number_bonds[atom_x] = dict_number_bonds[mol.atoms[atom_x].name]
+    if mode:
+        number_bonds = [10 for k in range(len(mol.atoms))]
+        list_atom_near = []
+        list_is_phenyl = []
 
-    #First pass
-    for atom_x in range(len(mol.atoms)):
-        atoms_near = find_atom_near(mol,mol.atoms[atom_x].pos)
-        list_atom_near.append(atoms_near)
 
-        if is_phenyl(mol,atoms_near):
-            for atom_near in range(len(atoms_near)):
-                if atoms_near[atom_near] and not(are_bonded(bonds,atom_x,atom_near)[0]):
-                    if mol.atoms[atom_near].name=="C":
-                        bonds.append([atom_x,atom_near,1.5])
-                        number_bonds[atom_x] = number_bonds[atom_x]-1.5
-                        number_bonds[atom_near] =number_bonds[atom_near]- 1.5
-                    else:
+
+        #Find number of bonds for each atom
+        for atom_x in range(len(mol.atoms)):
+            if mol.atoms[atom_x].name in dict_number_bonds:
+                number_bonds[atom_x] = dict_number_bonds[mol.atoms[atom_x].name]
+
+        #First pass
+        for atom_x in range(len(mol.atoms)):
+            atoms_near = find_atom_near(mol,mol.atoms[atom_x].pos)
+            list_atom_near.append(atoms_near)
+
+            if is_phenyl(mol,atoms_near):
+                for atom_near in range(len(atoms_near)):
+                    if atoms_near[atom_near] and not(are_bonded(bonds,atom_x,atom_near)[0]):
+                        if mol.atoms[atom_near].name=="C":
+                            bonds.append([atom_x,atom_near,1.5])
+                            number_bonds[atom_x] = number_bonds[atom_x]-1.5
+                            number_bonds[atom_near] =number_bonds[atom_near]- 1.5
+                        else:
+                            bonds.append([atom_x,atom_near,1])
+                            number_bonds[atom_x] = number_bonds[atom_x]-1
+                            number_bonds[atom_near] = number_bonds[atom_near]-1
+            else:
+                for atom_near in range(len(atoms_near)):
+                    if atoms_near[atom_near] and not(are_bonded(bonds,atom_x,atom_near)[0]):
                         bonds.append([atom_x,atom_near,1])
                         number_bonds[atom_x] = number_bonds[atom_x]-1
                         number_bonds[atom_near] = number_bonds[atom_near]-1
-        else:
+
+        #Second pass where the atoms that only have one neighbor are solved
+        for atom_1 in range(len(number_bonds)):
+            if mol.atoms[atom_1].name in dict_number_bonds:
+                if number_bonds[atom_1] > 0:
+                    atom_can_bond = []
+                    for atom_2 in range(len(number_bonds)):
+                        are_bond, bond_index = are_bonded(bonds,atom_1,atom_2)
+                        if are_bond and number_bonds[atom_2]>0:
+                            atom_can_bond.append([atom_2,bond_index])
+
+                    if len(atom_can_bond) == 1:
+                        #Only one atom, has to be bonded
+                        bonds[atom_can_bond[0][1]][2] += number_bonds[atom_1]
+                        number_bonds[atom_can_bond[0][0]] -= number_bonds[atom_1]
+                        number_bonds[atom_1] = 0
+
+        #Third pass where the bonds that are left are divided equally
+        for atom_1 in range(len(number_bonds)):
+            if mol.atoms[atom_1].name in dict_number_bonds:
+                if number_bonds[atom_1] > 0:
+                    atom_can_bond = []
+                    for atom_2 in range(len(number_bonds)):
+                        are_bond, bond_index = are_bonded(bonds,atom_1,atom_2)
+                        if are_bond and number_bonds[atom_2]>0:
+                            atom_can_bond.append([atom_2,bond_index])
+                    if len(atom_can_bond):
+                        bond_number = number_bonds[atom_1] / len(atom_can_bond)
+                        number_bonds[atom_1] = 0
+                        for atom_2 in range(len(atom_can_bond)):
+                            bonds[atom_can_bond[atom_2][1]][2] += bond_number
+                            number_bonds[atom_can_bond[atom_2][0]] -= bond_number
+
+
+        for atom_1 in range(len(number_bonds)):
+            if number_bonds[atom_1] < 0:
+                print("The atom {} has too much bonds".format(mol.atoms[atom_1].nickname))
+        #add 1 into bonds indexes
+        for bond in range(len(bonds)):
+            bonds[bond][0]+=1
+            bonds[bond][1]+=1
+
+    else:
+        for atom_x in range(len(mol.atoms)):
+            atoms_near = find_atom_near(mol,mol.atoms[atom_x].pos)
             for atom_near in range(len(atoms_near)):
                 if atoms_near[atom_near] and not(are_bonded(bonds,atom_x,atom_near)[0]):
-                    bonds.append([atom_x,atom_near,1])
-                    number_bonds[atom_x] = number_bonds[atom_x]-1
-                    number_bonds[atom_near] = number_bonds[atom_near]-1
-    #Second pass where the atoms that only have one neighbor are solved
-    for atom_1 in range(len(number_bonds)):
-        if mol.atoms[atom_1].name in dict_number_bonds:
-            if number_bonds[atom_1] > 0:
-                atom_can_bond = []
-                for atom_2 in range(len(number_bonds)):
-                    are_bond, bond_index = are_bonded(bonds,atom_1,atom_2)
-                    if are_bond and number_bonds[atom_2]>0:
-                        atom_can_bond.append([atom_2,bond_index])
-
-                if len(atom_can_bond) == 1:
-                    #Only one atom, has to be bonded
-                    bonds[atom_can_bond[0][1]][2] += number_bonds[atom_1]
-                    number_bonds[atom_can_bond[0][0]] -= number_bonds[atom_1]
-                    number_bonds[atom_1] = 0
-
-    #Third pass where the bonds that are left are divided equally
-    for atom_1 in range(len(number_bonds)):
-        if mol.atoms[atom_1].name in dict_number_bonds:
-            if number_bonds[atom_1] > 0:
-                atom_can_bond = []
-                for atom_2 in range(len(number_bonds)):
-                    are_bond, bond_index = are_bonded(bonds,atom_1,atom_2)
-                    if are_bond and number_bonds[atom_2]>0:
-                        atom_can_bond.append([atom_2,bond_index])
-                if len(atom_can_bond):
-                    bond_number = number_bonds[atom_1] / len(atom_can_bond)
-                    number_bonds[atom_1] = 0
-                    for atom_2 in range(len(atom_can_bond)):
-                        bonds[atom_can_bond[atom_2][1]][2] += bond_number
-                        number_bonds[atom_can_bond[atom_2][0]] -= bond_number
-
-
-    for atom_1 in range(len(number_bonds)):
-        if number_bonds[atom_1] < 0:
-            print("The atom {} has too much bonds".format(mol.atoms[atom_1].nickname))
-    #add 1 into bonds indexes
-    for bond in range(len(bonds)):
-        bonds[bond][0]+=1
-        bonds[bond][1]+=1
+                    bonds.append([atom_x+1,atom_near+1,1])
 
 
     return bonds
